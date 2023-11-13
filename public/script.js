193a31c6e03ed71797d3724809afe74fc130b43e
Adisa Ade <abubakradisa@gmail.com>
2023-11-13 16:08:18 +0100

    made some changes

--- public/script.js
+++ public/script.js
@@ -50,50 +50,32 @@ const boost = document.querySelector('.boost')
 
 
 button.addEventListener('click', shortenLink)
-    
+const inputUrl = document.getElementById('longurl')
 async function shortenLink(){         
-    const inputUrl = document.getElementById('longurl')
-    const err = document.querySelector('.err')
-    const linkToShorten = document.querySelector('.link-to-shorten')    
-    const shortenLink = document.querySelector('.shorten-link')    
-    
-    
     const url = await fetch (`https://is.gd/create.php?format=json&url=${inputUrl.value}`)         
     let response = await url.json()        
-    const link = document.querySelector('.link')
-    link.style.display = 'block'
-    const div = document.createElement('div')
-    const p = document.createElement('p')
-    const hr = document.createElement('hr')
-    const newDiv = document.createElement('div')
-    const pNewDiv = document.createElement('p')
-    const buttonNewDiv = document.createElement('button')
-    link.appendChild(div)
-    div.appendChild(p)
-    div.appendChild(hr)
-    div.appendChild(newDiv)
-    newDiv.appendChild(pNewDiv)
-    newDiv.appendChild(buttonNewDiv)
-    div.classList.add('flex','flex-col','space-y-3','md:space-y-0','md:flex-row','md:justify-between','md:items-center')
-    p.classList.add('link-to-shorten', 'text-darkViolet', 'break-words')
-    hr.classList.add('md:hidden', 'rule border-1', 'opacity-90', 'border-gray', 'w-full', 'my-6')
-    newDiv.classList.add('shorten-link', 'flex', 'flex-col', 'space-y-3', 'md:space-y-0', ' md:flex-row', 'md:items-center', 'md:space-x-3')
-    pNewDiv.classList.add('text-cyan', 'break-words')
-    buttonNewDiv.classList.add('button-copy', 'bg-cyan', 'p-5', 'md:p-2', 'lg:p-2', 'text-white', 'font-bold', 'rounded-md')
-    buttonNewDiv.textContent = 'Copy'
+    shortenApi(response)
+    error(response)
+}
     
-    linkToShorten.textContent = inputUrl.value
-    shortenLink.textContent = response.shorturl
+function error(data){
+    const err = document.querySelector('.err')
     const regex = /(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})(\.[a-zA-Z0-9]{2,})?/
     if (inputUrl.value == '' || inputUrl.value !== regex) {
-        err.textContent = response.errormessage
+        err.textContent = data.errormessage
         err.style.display ='block'
         inputUrl.classList.add('longurl')
     }
         
 }
-      
-     
+    function shortenApi(data) {
+        const link = document.querySelector('.link')
+        const linkToShorten = document.querySelector('.link-to-shorten')    
+        const shortenLink = document.querySelector('.shorten-link')        
+        link.style.display = 'block'
+        linkToShorten.textContent = inputUrl.value
+        shortenLink.textContent = data.shorturl    
+    } 
   
 //  copyButton.addEventListener('click', () => {
 //     newURL.select()
